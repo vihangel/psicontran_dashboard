d359dc6325ba67186bb1d4ca16fae7afced867be
Angel <47459464+vihangel@users.noreply.github.com>
2022-04-05 21:27:33 -0300

    .

---
+++ lib/data/models/user _list/response_user_list/response_user_list_model.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../user_list_model.dart';
+
+part 'response_user_list_model.g.dart';
+
+@JsonSerializable()
+class ResponseUserListModel {
+  final String? error;
+
+  final List<UserListModel>? users;
+
+  ResponseUserListModel({
+    this.error,
+    this.users,
+  });
+
+  factory ResponseUserListModel.fromJson(Map<String, dynamic> json) =>
+      _$ResponseUserListModelFromJson(json);
+  Map<String, dynamic> toJson() => _$ResponseUserListModelToJson(this);
+}
